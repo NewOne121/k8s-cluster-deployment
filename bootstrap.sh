3e2dddf9dfdbb3312752dc0a4b4f311b6916a8d8
Filipp Filippov <f.filippov7@gmail.com>
2020-04-27 19:14:32 +0300

    add controller services

--- bootstrap.sh
+++ bootstrap.sh
@@ -214,18 +214,18 @@ kubectl config set-cluster vi7-kubernetes \
     --certificate-authority=${CERTS_DIR}/CA/ca.pem \
     --embed-certs=true \
     --server=https://127.0.0.1:6443 \
-    --kubeconfig=kube-controller-manager.kubeconfig
+    --kubeconfig=${CONF_DIR}/kube-controller-manager.kubeconfig
 
 kubectl config set-credentials system:kube-controller-manager \
     --client-certificate=${CERTS_DIR}/controller-manager/kube-controller-manager.pem \
     --client-key=${CERTS_DIR}/controller-manager/kube-controller-manager-key.pem \
     --embed-certs=true \
-    --kubeconfig=kube-controller-manager.kubeconfig
+    --kubeconfig=${CONF_DIR}/kube-controller-manager.kubeconfig
 
 kubectl config set-context default \
     --cluster=vi7-kubernetes \
     --user=system:kube-controller-manager \
-    --kubeconfig=kube-controller-manager.kubeconfig
+    --kubeconfig=${CONF_DIR}/kube-controller-manager.kubeconfig
 
 kubectl config use-context default --kubeconfig=${CONF_DIR}/kube-controller-manager.kubeconfig
 
