6c64b4e3d1764ab24d7f9c501ca96c0accf17ba2
Filipp Filippov <f.filippov7@gmail.com>
2020-04-28 12:54:29 +0300

    fix config

--- bootstrap.sh
+++ bootstrap.sh
@@ -344,7 +344,6 @@ cp ${KUBECONFDIR}/kube-controller-manager.systemd.unit /etc/systemd/system/kube-
 cp ${CONF_DIR}/kube-scheduler.kubeconfig /var/lib/kubernetes/
 cp ${CONF_DIR}/kube-scheduler.kubeconfig /etc/kubernetes/config/kube-scheduler.kubeconfig
 cp ${KUBECONFDIR}/kube-scheduler.yaml /etc/kubernetes/config/kube-scheduler.yaml
-cp ${KUBECONFDIR}/kube-scheduler.yaml /var/lib/kubernetes/
 cp ${KUBECONFDIR}/kube-scheduler.systemd.unit /etc/systemd/system/kube-scheduler.service
 
 #Start controller services
